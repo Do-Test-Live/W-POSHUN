83720f8203dc55504c938831fd0f9502340ca5f6
Monoget <monoget2@gmail.com>
2023-04-16 13:28:20 +0600

    update all

--- admin/include/nav.php
+++ admin/include/nav.php
@@ -22,16 +22,6 @@
             </li>
             <li>
                 <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="flaticon-381-upload"></i>
-                    <span class="nav-text">Store</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="Add-Store">Add Store</a></li>
-                    <li><a href="Store">Store List</a></li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
                     <i class="flaticon-381-controls-3"></i>
                     <span class="nav-text">Product</span>
                 </a>
@@ -39,58 +29,6 @@
                     <li><a href="Add-Product">Add Product</a></li>
                     <li><a href="Product">Product List</a></li>
                 </ul>
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="flaticon-381-database-2"></i>
-                    <span class="nav-text">Course</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="Add-Course">Add Course</a></li>
-                    <li><a href="Course">Course List</a></li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="flaticon-381-notepad"></i>
-                    <span class="nav-text">Promo Code</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="Add-Promo-Code">Add Promo Code</a></li>
-                    <li><a href="Promo-Code">Promo Code List</a></li>
-                </ul>
-            </li>
-            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="flaticon-381-layer-1"></i>
-                    <span class="nav-text">Analysis</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="Product-Analysis">Product Analysis</a></li>
-                    <li><a href="Business-Analysis">Business Analysis</a></li>
-                    <li><a href="Store-Analysis">Store Analysis</a></li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="flaticon-381-heart"></i>
-                    <span class="nav-text">Order</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="Pending-Order">Pending Order</a></li>
-                    <li><a href="Confirm-Order">Confirm order</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="Report" class="ai-icon" aria-expanded="false">
-                    <i class="flaticon-381-settings-2"></i>
-                    <span class="nav-text">Report</span>
-                </a>
-            </li>
-            <li>
-                <a href="Customer">
-                    <i class="flaticon-381-internet"></i>
-                    <span class="nav-text">Customer</span>
-                </a>
             </li>
             <li>
                 <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
